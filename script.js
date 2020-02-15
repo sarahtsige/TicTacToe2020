a41ade9bca7d32279f7bbd933d24a12f3eef2131
sarahsaleh <sarahtsige@gmail.com>
2020-02-14 21:41:47 -0500

    fixed event listeners

--- script.js
+++ script.js
@@ -1,60 +1,66 @@
 
 let squares = document.querySelectorAll('.squares');
-squares.forEach(square => square.addEventListener("click", changeColor));
-
-let turn = 0
-function changeColor(e) {
-    console.log('click worked!')
-    //change color of the clicked item
-        e.target.style.backgroundColor = 'blue';
-        //remove event listener
-        e.target.removeEventListener('click', changeColor);
-        turn += 1;
-        console.log(turn);
-        if (turn%2) {
-            e.target.style.backgroundColor = "red";
-            e.target.removeEventListener('click', changeColor);
-        }   
-        checkWinner();
-}
-
-
-
-
-
-
-
 //add event listener for squares
+squares.forEach(square => square.addEventListener("click", changeColor));
+let turn = 0
+
 
 let winningArray = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
-    [0, 3, 6], 
+    [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
-    ];
- 
-    
+];
+
+function checkWinner() {
+}
 
 
 
+function changeColor(e) {
+    console.log(turn);
+    //change color of the clicked item
+    if (turn % 2 == 0) {
+        e.target.style.backgroundColor = "blue";
+    } else {
+        e.target.style.backgroundColor = "red";
+    }
+    //remove event listener
+    let clickedSquare = e.target;
+    clickedSquare.removeEventListener('click', changeColor);
+    turn += 1;
+    checkWinner();
+}
 
 
-    function checkWinner() {
-        if (
-            squares[0].style.backgroundColor == squares[1].style.backgroundColor &&  squares[0].style.backgroundColor ==  squares[2].style.backgroundColor
-        ) console.log (`${squares[0].style.backgroundColor} wins!`)
-        else if (
-            squares[3].style.backgroundColor == "red" &&
-            squares[4].style.backgroundColor == "red" &&
-            squares[5].style.backgroundColor == "red"
-        ) console.log ('Red wins!')
-        else if (
-            squares[6].style.backgroundColor == "red" &&
-            squares[7].style.backgroundColor == "red" &&
-            squares[8].style.backgroundColor == "red"
-        ) console.log ('Red wins!')
-    }    
+
+// function checkWinner() {
+//     if (
+//         squares[0].style.backgroundColor == squares[1].style.backgroundColor && squares[0].style.backgroundColor == squares[2].style.backgroundColor
+//     ) alert(`${squares[0].style.backgroundColor} wins!`)
+//     else if (
+//         squares[3].style.backgroundColor == squares[4].style.backgroundColor && squares[3].style.backgroundColor == squares[5].style.backgroundColor
+//     ) alert(`${squares[3].style.backgroundColor} wins!`)
+//     else if (
+//         squares[6].style.backgroundColor == squares[7].style.backgroundColor && squares[6].style.backgroundColor == squares[8].style.backgroundColor
+//     ) alert(`${squares[6].style.backgroundColor} wins!`)
+//     else if (
+//         squares[0].style.backgroundColor == squares[3].style.backgroundColor && squares[0].style.backgroundColor == squares[6].style.backgroundColor
+//     ) alert(`${squares[0].style.backgroundColor} wins!`)
+//     else if (
+//         squares[1].style.backgroundColor == squares[4].style.backgroundColor && squares[1].style.backgroundColor == squares[7].style.backgroundColor
+//     ) alert(`${squares[1].style.backgroundColor} wins!`)
+//     else if (
+//         squares[2].style.backgroundColor == squares[5].style.backgroundColor && squares[2].style.backgroundColor == squares[8].style.backgroundColor
+//     ) alert(`${squares[2].style.backgroundColor} wins!`)
+//     else if (
+//         squares[0].style.backgroundColor == squares[4].style.backgroundColor && squares[0].style.backgroundColor == squares[8].style.backgroundColor
+//     ) alert(`${squares[2].style.backgroundColor} wins!`)
+//     else if (
+//         squares[2].style.backgroundColor == squares[4].style.backgroundColor && squares[2].style.backgroundColor == squares[6].style.backgroundColor
+//     ) alert(`${squares[2].style.backgroundColor} wins!`)
+// }
