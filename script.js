b6eb7801b1b1a9d8665b4b0a767291bb9c6ef957
sarahsaleh <sarahtsige@gmail.com>
2020-02-15 17:44:18 -0500

    logic works and game annouces tie

--- script.js
+++ script.js
@@ -3,64 +3,54 @@ let squares = document.querySelectorAll('.squares');
 //add event listener for squares
 squares.forEach(square => square.addEventListener("click", changeColor));
 let turn = 0
+let blueSquares = []
+let redSquares = []
+
 
 
 let winningArray = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+    ['0', '1', '2'],
+    ['3', '4', '5'],
+    ['6', '7', '8'],
+    ['0', '3', '6'],
+    ['1', '4', '7'],
+    ['2', '5', '8'],
+    ['0', '4', '8'],
+    ['2', '4', '6']
 ];
 
 function checkWinner() {
+    for (let i=0; i < winningArray.length; i++) {
+        if(winningArray[i].every(e => blueSquares.includes(e))){
+            alert("Blue Wins!");
+            break;
+        } 
+        if(winningArray[i].every(e => redSquares.includes(e))){
+            alert("Red Wins!");
+            break;
+        }
+    }
 }
 
 
-
 function changeColor(e) {
-    console.log(turn);
-    //change color of the clicked item
+    //change color of the clicked item      
     if (turn % 2 == 0) {
         e.target.style.backgroundColor = "blue";
+        blueSquares.push(e.target.id);
+        console.log(blueSquares);
     } else {
         e.target.style.backgroundColor = "red";
+        redSquares.push(e.target.id);
+        console.log(redSquares);
     }
     //remove event listener
     let clickedSquare = e.target;
     clickedSquare.removeEventListener('click', changeColor);
     turn += 1;
+    console.log(turn);
     checkWinner();
+    if (turn == 9) {
+        alert("It's a tie!");
+    }
 }
-
-
-
-// function checkWinner() {
-//     if (
-//         squares[0].style.backgroundColor == squares[1].style.backgroundColor && squares[0].style.backgroundColor == squares[2].style.backgroundColor
-//     ) alert(`${squares[0].style.backgroundColor} wins!`)
-//     else if (
-//         squares[3].style.backgroundColor == squares[4].style.backgroundColor && squares[3].style.backgroundColor == squares[5].style.backgroundColor
-//     ) alert(`${squares[3].style.backgroundColor} wins!`)
-//     else if (
-//         squares[6].style.backgroundColor == squares[7].style.backgroundColor && squares[6].style.backgroundColor == squares[8].style.backgroundColor
-//     ) alert(`${squares[6].style.backgroundColor} wins!`)
-//     else if (
-//         squares[0].style.backgroundColor == squares[3].style.backgroundColor && squares[0].style.backgroundColor == squares[6].style.backgroundColor
-//     ) alert(`${squares[0].style.backgroundColor} wins!`)
-//     else if (
-//         squares[1].style.backgroundColor == squares[4].style.backgroundColor && squares[1].style.backgroundColor == squares[7].style.backgroundColor
-//     ) alert(`${squares[1].style.backgroundColor} wins!`)
-//     else if (
-//         squares[2].style.backgroundColor == squares[5].style.backgroundColor && squares[2].style.backgroundColor == squares[8].style.backgroundColor
-//     ) alert(`${squares[2].style.backgroundColor} wins!`)
-//     else if (
-//         squares[0].style.backgroundColor == squares[4].style.backgroundColor && squares[0].style.backgroundColor == squares[8].style.backgroundColor
-//     ) alert(`${squares[2].style.backgroundColor} wins!`)
-//     else if (
-//         squares[2].style.backgroundColor == squares[4].style.backgroundColor && squares[2].style.backgroundColor == squares[6].style.backgroundColor
-//     ) alert(`${squares[2].style.backgroundColor} wins!`)
-// }
